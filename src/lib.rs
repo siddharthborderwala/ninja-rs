0272ddda5448bc3414c0500b471b90d4621bf2c5
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-08-19 23:14:48 +0530

    build core parser data-structures and functions

---
+++ src/lib.rs
@@ -0,0 +1,132 @@
+#[derive(PartialEq, Debug)]
+pub enum ContentType {
+    Literal(String),
+    TemplateVariable(ExpressionData),
+    Tag(TagType),
+    Unrecognized,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum TagType {
+    ForTag,
+    IfTag,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct ExpressionData {
+    pub head: Option<String>,
+    pub variable: String,
+    pub tail: Option<String>,
+}
+
+pub fn check_symbol_string(candidate: &str, symbol: &str) -> bool {
+    candidate.contains(symbol)
+}
+
+pub fn check_matching_pair(candidate: &str, symbol1: &str, symbol2: &str) -> bool {
+    candidate.contains(symbol1) && candidate.contains(symbol2)
+}
+
+pub fn get_expression_data(input: &str) -> ExpressionData {
+    let (_h, i) = get_index_for_symbol(input, '{');
+    let head = input[0..i].to_string();
+    let (_j, k) = get_index_for_symbol(input, '}');
+    let variable = input[i + 1 + 1..k].to_string();
+    let tail = input[k + 1 + 1..].to_string();
+
+    ExpressionData {
+        head: Some(head),
+        variable,
+        tail: Some(tail),
+    }
+}
+
+pub fn get_index_for_symbol(input: &str, symbol: char) -> (bool, usize) {
+    let mut characters = input.char_indices();
+    let mut does_exist = false;
+    let mut index = 0;
+
+    while let Some((c, d)) = characters.next() {
+        if d == symbol {
+            does_exist = true;
+            index = c;
+            break;
+        }
+    }
+
+    (does_exist, index)
+}
+
+pub fn get_content_type(input_line: &str) -> ContentType {
+    let is_tag_expression = check_matching_pair(&input_line, "{%", "%}");
+    let is_for_tag = check_symbol_string(&input_line, "for")
+        && check_symbol_string(&input_line, "in")
+        || check_symbol_string(&input_line, "endfor");
+    let is_if_tag =
+        check_symbol_string(&input_line, "if") || check_symbol_string(&input_line, "endif");
+
+    let is_template_variable = check_matching_pair(&input_line, "{{", "}}");
+
+    if is_tag_expression && is_for_tag {
+        ContentType::Tag(TagType::ForTag)
+    } else if is_tag_expression && is_if_tag {
+        ContentType::Tag(TagType::IfTag)
+    } else if is_template_variable {
+        let content = get_expression_data(&input_line);
+        ContentType::TemplateVariable(content)
+    } else if !is_tag_expression && !is_template_variable {
+        ContentType::Literal(input_line.to_string())
+    } else {
+        ContentType::Unrecognized
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_literal_test() {
+        let s = "<h1>Hello World</h1>";
+        assert_eq!(ContentType::Literal(s.to_string()), get_content_type(s));
+    }
+
+    #[test]
+    fn check_template_var_test() {
+        let content = ExpressionData {
+            head: Some("Hi ".to_string()),
+            variable: "name".to_string(),
+            tail: Some(", welcome".to_string()),
+        };
+        assert_eq!(
+            ContentType::TemplateVariable(content),
+            get_content_type("Hi {{name}}, welcome")
+        );
+    }
+
+    #[test]
+    fn check_for_tag_test() {
+        assert_eq!(
+            ContentType::Tag(TagType::ForTag),
+            get_content_type("{% for name in names %}, welcome")
+        );
+    }
+
+    #[test]
+    fn check_if_tag_test() {
+        assert_eq!(
+            ContentType::Tag(TagType::IfTag),
+            get_content_type("{% if name == 'Bob' %}")
+        );
+    }
+
+    #[test]
+    fn check_symbol_string_test() {
+        assert_eq!(true, check_symbol_string("{{Hello}}", "{{"));
+    }
+
+    #[test]
+    fn check_get_index_for_symbol_test() {
+        assert_eq!((true, 3), get_index_for_symbol("Hi {name}, welcome", '{'));
+    }
+}
